5095e02052954098f5cf1ed72e2eba94bd3fe3ec
basswolfe <wolfe.design.dev@gmail.com>
2020-10-24 13:10:53 -0400

    add firebase and setup login component

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside>
+    <h3>Sign In Anonymously</h3>
+    <button @click="auth.signInAnonymousely()">Sign In</button>
+  </aside>
+</template>
+
+<script>
+import { auth } from '../firebase'
+
+export default {
+  data() {
+    return {
+      auth,
+    }
+  }
+}
+</script>
